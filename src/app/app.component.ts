e8b939fe5e3f23eba883a780fe461363dc864d7e
Matteo Crosta <matteo_crosta@fastwebnet.it>
2017-09-19 00:13:10 +0200

    It works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('mymap') mapDiv;
+
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiZHozMTY0MjQiLCJhIjoiNzI3NmNkOTcyNWFlNGQxNzU2OTA1N2EzN2FkNWIwMTcifQ.NS8KWg47FzfLPlKY0JMNiQ';
+
+    let map = new mapboxgl.Map({
+      container: this.mapDiv.nativeElement,
+      style: 'mapbox://styles/mapbox/light-v9',
+      zoom: 5,
+      center: [-78.880453, 42.897852]
+    });
+  }
+
 }
